feebb2f27c63b88b933d2210695af4b2ebd43126
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-04-22 11:30:02 -0400

    Remove user function added to dynamic form.

--- pyramid/src/Screens/Setup.js
+++ pyramid/src/Screens/Setup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import { Button, InputItem, List } from '@ant-design/react-native';
-
+import { InputItem } from '@ant-design/react-native';
+import { Formik } from 'formik';
 
 export default function Setup(props) {
     const { navigation } = props
@@ -9,6 +9,9 @@ export default function Setup(props) {
         pyramid_height: 0,
         users: []
     }
+    const createPlayer = () => ({
+        name: ''
+    });
     return (
         <View style={styles.container}>
             <Text style={{color: '#888', fontSize: 50, top:-100}}>Ingrese altura:</Text>
@@ -18,13 +21,41 @@ export default function Setup(props) {
                     data.pyramid_height = value
                 }}
             >
-            Altura
+                Altura
             </InputItem>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Home')}
-                style={{ backgroundColor: '#d1625a', padding: 10, borderRadius: 5 }}>
-                <Text style={{ fontSize: 20, color: '#fff' }}>Jugar</Text>
-            </TouchableOpacity>
+            <Formik
+                initialValues={{ players: [], }}
+                onSubmit={values => console.log(values)}
+            >
+            {({ handleChange, handleSubmit, values, setFieldValue }) => (
+                <View>
+                <Text>Ingresa jugadores</Text>
+                {values.players.map(({ name }, index) => (
+                    <InputItem
+                        placeholder="Ingresa nombre"
+                        key={index}
+                        onChangeText={handleChange(`players[${index}].name`)}
+                        value={values.players[index].name}
+                    />
+                ))}
+                <TouchableOpacity
+                    onPress={() => setFieldValue('players', [...values.players, createPlayer()])}
+                    style={{ backgroundColor: '#506280', padding: 10, alignSelf:"center", borderRadius: 5 }}>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>Agregar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setFieldValue('players', [...values.players.slice(0,-1)])}
+                    style={{ backgroundColor: '#506280', padding: 10, alignSelf:"center", borderRadius: 5 }}>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>Quitar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={handleSubmit}
+                    style={{ backgroundColor: '#d1625a', padding: 10, alignSelf:"center", borderRadius: 5 }}>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>A jugar!</Text>
+                </TouchableOpacity>
+                </View>
+            )}
+            </Formik>
         </View>
     );
 }
