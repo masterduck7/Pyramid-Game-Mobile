259edb4bc68633e1714f58db82a7c6e9ae82e6c6
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-05-05 18:51:09 -0400

    Fix instructions position

--- pyramid/src/Screens/Instructions.js
+++ pyramid/src/Screens/Instructions.js
@@ -8,13 +8,13 @@ export default class Instructions extends Component {
         return (
             <View style={styles.container}>
                 <ImageBackground source={background} resizeMethod="resize" style={styles.image}>
-                <Text style={{color: '#474442', fontSize: 40, top: 150}}>INSTRUCCIONES</Text>
+                <Text style={{color: '#474442', fontSize: 43, left: -5, top: 150}}>INSTRUCCIONES</Text>
                 <View style={{flex: 1, width: 300, height: 100, left: -15}}>
                 <Image source={Steps} resizeMode="stretch" style={{width: 400, height: 250, top: 200, left: -40}} /> 
                 </View>
                 <TouchableOpacity
                 onPress={() => this.props.navigation.navigate('Setup')}
-                style={{ borderColor: '#474442' , padding: 15, borderWidth: 2, top: -240, left: 100 }}>
+                style={{ borderColor: '#474442' , padding: 15, borderWidth: 2, top: -230, left: 125 }}>
                     <Text style={{ fontSize: 20, color: '#474442' }}>JUGAR</Text>
                 </TouchableOpacity>
                 </ImageBackground>
